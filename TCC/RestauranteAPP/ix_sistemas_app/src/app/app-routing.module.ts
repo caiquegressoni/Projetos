8bea0795d37cbeee6cc0f3822665037a5a1eb497
caiquegressoni@gmail.com <caiquegressoni@gmail.com>
2021-05-06 12:10:54 -0300

    Iniciado o app

---
+++ TCC/RestauranteAPP/ix_sistemas_app/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+    {
+    path: 'cardapio',
+    loadChildren: () => import('./pages/cardapio/cardapio.module').then( m => m.CardapioPageModule)
+  },
+    {
+    path: 'promocao',
+    loadChildren: () => import('./pages/promocao/promocao.module').then( m => m.PromocaoPageModule)
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
